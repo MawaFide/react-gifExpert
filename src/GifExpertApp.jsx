6dafe55af2c74e242ca342541f7b1bd1b69530c9
Fidel Abreu Souza <fidel02@nauta.cu>
2024-12-10 14:38:43 -0500

    11. Validar que sean únicos los nombres

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -5,6 +5,9 @@ export const GifExpertApp = () => {
     const [categories, setCategories] = useState([]);
 
     const onAddCoategory = (onNewCategory) => {
+        //comprobar si esxiste la categoria para no agregarla de nuevo
+        if (categories.includes(onNewCategory)) return;
+
         setCategories([onNewCategory, ...categories]);
         console.log(onNewCategory);
     };
